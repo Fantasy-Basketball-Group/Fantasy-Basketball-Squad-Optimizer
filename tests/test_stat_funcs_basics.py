af7f168246f6934cf331ee17fe6a3d57392ceca5
Mack Cooper <maqenzi@gmail.com>
2020-08-16 18:16:49 -0700

    formatting in black, remove stray print in unit test

--- tests/test_stat_funcs_basics.py
+++ tests/test_stat_funcs_basics.py
@@ -9,10 +9,9 @@ df = pd.read_csv("data/test_data_set.csv")
 
 # test to find player successfully
 def test_get_player_success():
-    avail = df.copy() # generate duplicate data
+    avail = df.copy()  # generate duplicate data
     player_name = "Stephen Curry"
     player_data = sf.find_player(player_name, avail)
-    print(player_data.values[0])
     assert len(player_data.values) == 1  # gets a single player
     assert player_data["Player"].values[0] == player_name  # name is correct
     assert player_data["Pos"].values[0] == "PG"  # position is correct
@@ -34,7 +33,7 @@ def test_get_player_success():
 
 # test that a player returns an empty data frame
 def test_get_player_failure():
-    avail = df.copy() # generate duplicate data
+    avail = df.copy()  # generate duplicate data
     player_name = "Steven Curry"
     player_data = sf.find_player(player_name, avail)
     assert player_data.empty == True
@@ -42,7 +41,7 @@ def test_get_player_failure():
 
 # test that mean player function produces means of all stats
 def test_means_player_produces_means():
-    avail = df.copy() # generate duplicate data
+    avail = df.copy()  # generate duplicate data
     mean_player = sf.mean_player(avail)
     assert len(mean_player.values) == 1  # produces a single player
     assert mean_player["Player"].values[0] == "Mr Mean"  # name is correct
@@ -61,13 +60,18 @@ def test_means_player_produces_means():
 
 # test that draft player function removes a player from the given roster and places it into an empty roster
 def test_draft_player_to_empty_roster_success():
-    empty_roster = pd.DataFrame() # generate empty roster
-    assert empty_roster.empty == True # roster is actually empty
-    avail = df.copy() # generate duplicate data
+    empty_roster = pd.DataFrame()  # generate empty roster
+    assert empty_roster.empty == True  # roster is actually empty
+    avail = df.copy()  # generate duplicate data
     player_name = "Stephen Curry"
     new_roster = sf.draft_player(player_name, empty_roster, avail)
-    assert len(avail.values) == len(df.values) - 1 # available players is one less than before
-    assert sf.find_player("Stephen Curry", avail).empty == True # the drafted played isn't in available players anymore
-    assert len(new_roster.values) == 1 # roster has one player
-    assert new_roster["Player"].values[0] == "Stephen Curry" # that player is the one drafted
-    
+    assert (
+        len(avail.values) == len(df.values) - 1
+    )  # available players is one less than before
+    assert (
+        sf.find_player("Stephen Curry", avail).empty == True
+    )  # the drafted played isn't in available players anymore
+    assert len(new_roster.values) == 1  # roster has one player
+    assert (
+        new_roster["Player"].values[0] == "Stephen Curry"
+    )  # that player is the one drafted
